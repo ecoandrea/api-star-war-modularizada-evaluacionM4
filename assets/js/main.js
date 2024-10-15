95d24d8a99668f5ce0abc14e634f8ce10c977e60
Cristina Zamora <ecoandrea@gmail.com>
2024-10-15 00:58:22 -0300

    feat:cambio de colores de iconos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,31 @@
 
 import { fetchCharactersGenerator } from './src/utils/characterGenerator.js'
 
-function createCharacterCard(character) {
+
+async function displayCharacter(containerId, characterGenerator, range) {
+    const container = document.getElementById(containerId);
+    try {
+        const character = await characterGenerator.next();
+        if (!character.done) {
+            const cardHTML = createCharacterCard(character.value, range);
+            container.innerHTML += cardHTML;
+        }
+    } catch (error) {
+        console.error(error);
+        container.innerHTML += `<p>Error loading character data.</p>`;
+    }
+}
+
+
+function createCharacterCard(character, range) {
+    const iconColorClass = range === "1-5" ? "red" :
+                            range === "6-11" ? "success" :
+                            range === "12-17" ? "danger" :
+                            "";
+
     return `
         <div class="single-timeline-content">
-            <div class="timeline-icon">
+            <div class="timeline-icon ${iconColorClass}">
                 <i class="fas fa-user"></i>
             </div>
             <div class="timeline-text">
@@ -16,36 +37,25 @@ function createCharacterCard(character) {
     `;
 }
 
-async function displayCharacter(containerId, characterGenerator) {
-    const container = document.getElementById(containerId);
-    try {
-        const character = await characterGenerator.next();
-        if (!character.done) {
-            const cardHTML = createCharacterCard(character.value);
-            container.innerHTML += cardHTML;
-        }
-    } catch (error) {
-        console.error(error);
-        container.innerHTML += `<p>Error loading character data.</p>`;
-    }
-}
 
 function initializeCharacterFetch() {
     document.querySelectorAll('.timeline-item').forEach(item => {
-        console.log("lluegue")
         const [start, end] = item.getAttribute('data-range').split('-').map(Number);
-
         const containerId = `range-${start}-${end}`;
         const characterGenerator = fetchCharactersGenerator(start, end);
+        const range = item.getAttribute('data-range');
 
         item.addEventListener('click', async () => {
-            await displayCharacter(containerId, characterGenerator);
+            await displayCharacter(containerId, characterGenerator, range);
         });
     });
 }
 
+// Inicializar la carga de personajes cuando el documento esté listo
+document.addEventListener('DOMContentLoaded', initializeCharacterFetch);
+
+
 // Llama a la función de inicialización al cargar el script
-initializeCharacterFetch();
 
 
 
